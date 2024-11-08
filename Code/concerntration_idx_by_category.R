06381edad776849fafe3f73c764f88245c923b7b
Yang Ju <juyangnju@gmail.com>
2024-11-08 20:30:56 +0800

    Add files via upload

--- Code/concerntration_idx_by_category.R
+++ Code/concerntration_idx_by_category.R
@@ -1,13 +1,14 @@
 library(rineq)
 # set to the correct working directory
 setwd('D:\\Box Sync\\Toxic Tides\\ToxicTides_US\\Code_Data_upload')
+
+
 urban = 2 # 1 urban, 0 non-urban, 2 all
-pw = 0 # 0 unweighted, 1 weighted
+pw = 0 # 0 unweighted by population, 1 weighted by population
 
 # Define the output directory path
 dir_path <- "Results/concerntration_index/"
-
-# Check if the directory exists
+# Check if the directory exists, create one if not
 if (!file.exists(dir_path)) {
   # Create the directory if it does not exist
   dir.create(dir_path, recursive = TRUE)
@@ -16,6 +17,8 @@ if (!file.exists(dir_path)) {
   cat("Directory already exists:", dir_path, "\n")
 }
 
+# calculate the concentration index for each facility category and selected
+# socioeconomic variables
 for (cid in cbind(1,2,3,4,5,6,7,8,9,10,11,18)){
   df = read.csv(sprintf("Data/analysis_sum_EAE_1000m_buffer_US_%s_25.csv",cid))
   df$exposed_205 = 0
@@ -33,12 +36,13 @@ for (cid in cbind(1,2,3,4,5,6,7,8,9,10,11,18)){
   df['est_pct_native']=100-df['est_pct_native']
   df['est_pct_otherpoc']=100-df['est_pct_otherpoc']
   df['pct_novote'] = 100-df['pct_novote']
+  
   # if unweighted, assign population=1
   if (pw==0){
    df$tot_pop=1 
   }
   
-  #########################EAE#########################
+  #########################concentration index for EAE#########################
   res = data.frame()
   idx=1
   for (exp in cbind('RCP85_Annual_2050','RCP85_Annual_2100')){
@@ -58,14 +62,16 @@ for (cid in cbind(1,2,3,4,5,6,7,8,9,10,11,18)){
       res[idx,'nobs']=nrow(sub_df)
       idx=idx+1
       out = cbind(ci$fractional_rank,ci$outcome)
+      # store the detailed values from concentration index calculation for plotting concerntration curves
       write.csv(out,sprintf("Results/concerntration_index/ci_details_eae_%s_%s_category%s_pw%s_urban%s.csv",exp,var,cid,pw,urban)) # entire US
     }
   }
   res$lower = res$CI-1.96*res$se
   res$upper = res$CI+1.96*res$se
+  # store the concentration index values
   write.csv(res, sprintf("Results/concerntration_index/ci_eae_category%s_pw%s_urban%s.csv",cid,pw,urban), row.names=FALSE)
   
-  ########################n_exposed#########################
+  ########################concentration index for number of exposed facilities#########################
   df = read.csv(sprintf("Data/analysis_count_1000m_buffer_US_%s_25.csv",cid))
   df$exposed_205 = 0
   df$exposed_205[df$RCP85_Annual_2050>0]=1
@@ -107,11 +113,13 @@ for (cid in cbind(1,2,3,4,5,6,7,8,9,10,11,18)){
       res[idx,'nobs']=nrow(sub_df)
       idx=idx+1
       out = cbind(ci$fractional_rank,ci$outcome)
+      # store the detailed values from concentration index calculation for plotting concerntration curves
       write.csv(out,sprintf("Results/concerntration_index/ci_details_count_%s_%s_category%s_pw%s_urban%s.csv",exp,var,cid,pw,urban)) #entire US
     }
   }
   res$lower = res$CI-1.96*res$se
   res$upper = res$CI+1.96*res$se
+  # store the concentration index values
   write.csv(res, sprintf("Results/concerntration_index/ci_count_category%s_pw%s_urban%s.csv",cid,pw,urban), row.names=FALSE)
 }
 ###end
